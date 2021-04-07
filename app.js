22c63d3722553644861dc044d8081aed19eecfa9
rhinoback <74320177+rhinoback@users.noreply.github.com>
2021-04-06 17:23:10 -0700

    app committee

--- app.js
+++ app.js
@@ -3,32 +3,16 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // Use D3 to select the table body
-var tbody = d3.select("tbody");
+var tbody = d3.select(".table");
 
 // Use D3 to select the table
-var table = d3.select("table");
+var table = d3.selectAll("td");
 
-// Use D3 to set the table class to `table table-striped`
-table.attr("class", "table table-striped");
-
-// BONUS: Dynamic table
-// Loop through an array of grades and build the entire table body from scratch
-
-var tableData = data;
-
-// Iterate through each student/grade pair
-tableData.forEach(([datetime, city, state, country, shape, durationMinutes, comments]) => {
-
-  // Append one table row per student/grade
-  var row = tbody.append("tr");
-
-  // append one cell for the student and one cell for the grade
-  row.append("td").text(datetime);
-  row.append("td").text(city);
-  row.append("td").text(state);
-  row.append("td").text(country);
-  row.append("td").text(shape);
-  row.append("td").text(durationMinutes);
-  row.append("td").text(comments);
-
-});
+//Loop through and make the table
+tableData.forEach((AlienAwesome) => {
+    console.log(AlienAwesome)
+    var row= tbody.append("tr")
+    Object.entries(AlienAwesome).forEach(function([key, value]){
+    var cell = row.append("td");
+    cell.text(value)
+    });
