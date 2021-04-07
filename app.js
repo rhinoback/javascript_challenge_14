51a2d2b65411db67f7e95a7a0260816ae4ad5550
rhinoback <74320177+rhinoback@users.noreply.github.com>
2021-04-06 17:38:44 -0700

    commited commitee

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var tbody = d3.select(".table");
 // Use D3 to select the table
 var table = d3.selectAll("td");
 
-//Loop through and make the table
+// Use D3 to set the table class to `table table-striped`
+// BONUS: Dynamic table
+// Loop through an array of grades and build the entire table body from scratch
+// Iterate through each student/grade pair
 tableData.forEach((AlienAwesome) => {
     console.log(AlienAwesome)
     var row= tbody.append("tr")
@@ -16,3 +19,9 @@ tableData.forEach((AlienAwesome) => {
     var cell = row.append("td");
     cell.text(value)
     });
+})
+
+  
+
+
+
