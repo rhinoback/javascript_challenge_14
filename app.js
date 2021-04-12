ca9bdc2dc6de39d97a3688a4c7023f8e3964def0
rhinoback <74320177+rhinoback@users.noreply.github.com>
2021-04-11 17:30:53 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,51 +3,35 @@ var tableData = data;
 
 // YOUR CODE HERE!
 // Use D3 to select the table body
-var tbody = d3.select(".table");
+var tbody = d3.select("tbody");
 
 // Use D3 to select the table
 var table = d3.selectAll("td");
 
-//Create a basic HTML web page or use the [index.html](StarterCode/index.html) file provided (we recommend building your own custom page!).
-//Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-// * Make sure you have a column for `date/time`, `city`, `state`, `country`, `shape`, and `comment` at the very least
-tableData.forEach((AlienAwesome) => {
-    console.log(AlienAwesome)
-    var row= tbody.append("tr")
-    Object.entries(AlienAwesome).forEach(function([key, value]){
-    var cell = row.append("td");
-    cell.text(value)
+function tableBuilding(data){
+    tbody.html("")
+    data.forEach((rowData)=>{
+        let row=  tbody.append("tr");
+        Object.values(rowData).forEach((val)=>{
+            let cell = row.append("td");
+            cell.text(val);
+        }
+        );
     });
-})
+}
 
+function runEnter(){
+    let inputElement = d3.select("#datetime").property("value");
+    let filtered = tableData;
+    
+    if (inputElement) {
+        filtered = filtered.filter(row=>row.datetime===inputElement);
+    }
+    tableBuilding(filtered);
+}
 
-// //* Use a date form in your HTML document and write JavaScript code that will listen for events and search through the `date/time` column to find rows that match user input.
-// var button = d3.select("#filter-btn");
-
-// var form = d3.select("#datetime");
-
-// button.on("click", runEnter);
-// form.on("submit", runEnter);
-
-// function runEnter() {
-//     d3.event.preventDefault();
-
-//     var inputElement = d3.select("datetime");
-
-//     var inputValue = inputElement.property("value");
-
-//     console.log(inputValue);
-
-
-// }
-// tableData.forEach((AlienAwesome) => {
-//     console.log(AlienAwesome)
-//     var row= tbody.append("tr")
-//     Object.entries(AlienAwesome).forEach(function([key, value]){
-//     var cell = row.append("td");
-//     cell.text(value)
-//     });
-
-// d3.select("h1>span").text(inputValue);
-
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", runEnter);
+// Build the table when the page loads
+tableBuilding(tableData);
 
